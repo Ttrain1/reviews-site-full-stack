f27de6bed48cc1868fd412520df2d7843eb69144
WaynePL <wayneplivingston@gmail.com>
2018-10-19 14:20:53 -0400

    renamed packages

--- src/main/java/org/wecancodeit/reviewssitefullstack/model/Category.java
+++ src/main/java/org/wecancodeit/reviewssitefullstack/model/Category.java
@@ -1,8 +1,12 @@
 package org.wecancodeit.reviewssitefullstack.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Category {
@@ -12,11 +16,19 @@ public class Category {
 	private Long id;
 	private String name;
 
+	@OneToMany
+	(mappedBy="category")
+	private Set<Review> reviews = new HashSet<Review>();
+	
 	public Category() {
 	}
 
 	public Category(String name) {
 		this.name = name;
+	}
+
+	public Set<Review> getReviews() {
+		return reviews;
 	}
 
 	public Long getId() {
