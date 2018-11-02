1b353a2105d79c7e07c0e6a6301b5686138d0fa8
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-11-02 10:53:01 -0400

    added api and tag

---
+++ src/main/java/org/wecancodeit/reviewssitefullstack/model/Tag.java
@@ -0,0 +1,22 @@
+package org.wecancodeit.reviewssitefullstack.model;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import antlr.collections.List;
+
+@Entity
+public class Tag {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	@ManyToMany
+	(mappedBy="review")
+	private Collection<Review> reviews; 
+}
